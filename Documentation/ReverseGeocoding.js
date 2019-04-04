ffdc6dc6c929eabc3d64667334ed1bf0d9bfd91b
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2019-04-04 13:30:55 +0530

    Create ReverseGeocoding.js

---
+++ Documentation/ReverseGeocoding.js
@@ -0,0 +1,57 @@
+describe('reverseGeocode', () => {
+  test('with minimal config', () => {
+    geocoding.reverseGeocode({
+      query: [15, 14]
+    });
+    expect(tu.requestConfig(geocoding)).toEqual({
+      method: 'GET',
+      path: '/geocoding/v5/:mode/:query.json',
+      params: {
+        query: [15, 14],
+        mode: 'mapbox.places'
+      },
+      query: {}
+    });
+  });
+
+  test('with all config options', () => {
+    geocoding.reverseGeocode({
+      query: [15, 14],
+      mode: 'mapbox.places-permanent',
+      countries: ['AO', 'AR'],
+      types: ['country', 'region'],
+      bbox: [1, 2, 3, 4],
+      limit: 3,
+      language: ['de', 'bs'],
+      reverseMode: 'distance'
+    });
+    expect(tu.requestConfig(geocoding)).toEqual({
+      method: 'GET',
+      path: '/geocoding/v5/:mode/:query.json',
+      params: {
+        query: [15, 14],
+        mode: 'mapbox.places-permanent'
+      },
+      query: {
+        country: ['AO', 'AR'],
+        types: ['country', 'region'],
+        bbox: [1, 2, 3, 4],
+        limit: 3,
+        language: ['de', 'bs'],
+        reverseMode: 'distance'
+      }
+    });
+  });
+});
+
+
+"""
+geocodingClient.reverseGeocode({
+  query: [-95.4431142, 33.6875431],
+  limit: 2
+})
+  .send()
+  .then(response => {
+    // GeoJSON document with geocoding matches
+    const match = response.body;
+  });
